76bd856e71920c6c452596715f31d2eefea3e25c
Reenta <reenta.kim@gmail.com>
2024-07-26 15:50:51 +0900

    useNavigate로 홈페이지로 돌아가자

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -4,7 +4,7 @@ const ProductCard = ({item}) => {
     return (
         <div>
             <img src={item?.img} alt="img" className={'cloth-img'}/>
-            <div>Conscious Choice</div>
+            <div>{item?.choice === true ? 'Conscious Choice' : ''}</div>
             <div>{item?.title}</div>
             <div>${item?.price}</div>
             <div>{item?.new === true ? '신제품' : ''}</div>
