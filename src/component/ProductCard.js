389d0d00bc5113fdf039efd6ac3d7bc9530f7bb0
Reenta <reenta.kim@gmail.com>
2024-07-26 15:02:06 +0900

    API 부르기

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProductCard = () => {
+    return (
+        <div>
+            <img src="https://images.chosun.com/resizer/twzW6qXLO00-OHqi9YYBlBDx07E=/530x810/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/D3D6EZEZSDHJITXENAJ3WKC7TY.jpg" alt="img"/>
+            <div>Conscious Choice</div>
+            <div>밸티드 트윌 코트</div>
+            <div>$999.9</div>
+            <div>신제품</div>
+        </div>
+    );
+};
+
+export default ProductCard;
